a0ebcb90088249064a2f1050d2b72f68a71706f3
Flynn <gaoentao2@gmail.com>
2026-01-29 16:35:38 +1100

    feat: database connection verified with db pull

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,14 @@
-// 1. 这一行很重要，它负责把 .env 里的变量读入内存
 import "dotenv/config"; 
-// 2. 如果 "prisma/config" 报错，尝试改为 "@prisma/config"
 import { defineConfig } from "@prisma/config"; 
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    // ⬇️ 就是加下面这一行，告诉 Prisma 种子脚本的位置
+    seed: "tsx prisma/seed.ts", 
   },
   datasource: {
-    // 3. 确保这里的变量名和 .env 里的一模一样
     url: process.env.DATABASE_URL, 
   },
 });
